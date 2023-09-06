e92c0467e4f7c97b8c2bfaa6dc054bf81b46345e
Sanjeet <Sanjeetsinghchahal933@gmail.com>
2023-09-06 15:03:08 +0530

    Add changes

---
+++ src/server/1-matchesPerYear.js
@@ -0,0 +1,14 @@
+function matchesPerYear(matches){
+    let output = {}
+    matches.forEach((element) => {
+        let year = element.season
+        if(!output[year]){
+            output[year] = 1
+        }else{
+            output[year]++;
+        }
+    })
+    return output
+}
+
+module.exports = matchesPerYear
---
+++ src/server/3-extraRuns2016.js
@@ -0,0 +1,22 @@
+function extraRun(matchesdata,delevryData,year){
+    let result = {}
+    let matchesRequired = []
+    matchesdata.forEach((ele) => {
+        if(ele.season === year){
+            matchesRequired.push(ele.id)
+        }
+    })
+    delevryData.forEach((ele) => {
+        if(matchesRequired.includes(ele.match_id)){
+            let bowlingTeam = ele.bowling_team
+            let extraRun = Number(ele.extra_runs)
+            if(!result[bowlingTeam]){
+                result[bowlingTeam] = 0
+            }
+            result[bowlingTeam] += extraRun     
+        }
+    })
+    return result  
+}
+module.exports = extraRun
+
